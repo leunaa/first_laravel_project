11ce637126de34aed433d811a8ad8005022917f2
Leunamme Atutubo <leunammerosev.atutubo@gmail.com>
2024-10-26 16:01:18 +0800

    Tag

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -14,4 +14,7 @@ class Job extends Model{
     public function employer() {
         return $this->belongsTo(Employer::class);
     }
+    public function tags() {
+        return $this->belongsToMany(Tag::class);
+    }
 }
